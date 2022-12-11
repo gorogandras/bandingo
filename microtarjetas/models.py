13a9311b4ab6939435e50070449ed2f9f016828c
gorogandras <gorog.andras.gabor@gmail.com>
2022-12-11 16:01:24 +0100

    modifictations

--- microtarjetas/models.py
+++ microtarjetas/models.py
@@ -13,7 +13,7 @@ class Card(models.Model):
     word_es = models.CharField(max_length = 30)
     deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
     
-    def get_absolute_url(self):
+    def get_absolute_url(self): #This one serves to redirect to homepage if we submit data
         return "/cards"
 
     def __str__(self):
